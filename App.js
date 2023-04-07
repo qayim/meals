4bc2a044427de882e5bc16c3f23518fee55543eb
Qayim <200919@student.upm.edu.my>
2023-04-07 14:15:52 +0800

    redux update

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import "react-native-gesture-handler";
 
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoriteScreen from "./screens/FavoriteScreen";
-import FavoritesContextProvider from "./store/context/favorites-context";``
+//import FavoritesContextProvider from "./store/context/favorites-context";
+import { store } from "./store/redux/store";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
-export default function App(){
+export default function App() {
   return (
     <>
       <StatusBar style="dark" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -42,11 +44,8 @@ export default function App(){
             <Stack.Screen name="Favorites" component={FavoriteScreen} />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,5 @@
-import { useLayoutEffect, useContext } from "react";
+import { useLayoutEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   Text,
   View,
@@ -14,22 +15,27 @@ import MealDetails from "../components/MealDetail/MealDetails";
 import List from "../components/List";
 import IconButton from "../components/IconButton";
 import { MEALS } from "../data/dummy-data";
-import { FavoritesContext } from "../store/context/favorites-context";
+import { addFavorite, removeFavorite } from "../store/redux/favorites";
 
 function MealDetailScreen({ route, navigation }) {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  //const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+  const dispatch = useDispatch();
+
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+  const mealIsFavorite = favoriteMealIds.includes(mealId);
 
   function changeFavoriteStatusHandler() {
     if (mealIsFavorite) {
-      favoriteMealsCtx.removeFavorite(mealId);
+      //favoriteMealsCtx.removeFavorite(mealId);
+      dispatch(removeFavorite({ id: mealId }));
       console.log("Remove favorite: " + mealId);
     } else {
-      favoriteMealsCtx.addFavorite(mealId);
+      //favoriteMealsCtx.addFavorite(mealId);
+      dispatch(addFavorite({ id: mealId }));
       console.log("Add favorite: " + mealId);
     }
     console.log("Header button pressed");
